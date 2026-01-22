1d6b80ee1cf15b501be013361fb5af5f722ec683
Thomas Wilson <86075837+thomasw005@users.noreply.github.com>
2026-01-21 19:50:19 -0500

    Update canny.c

--- canny.c
+++ canny.c
@@ -1,162 +1,104 @@
-#include <stdio.h>                  /*  Marr-Hildreth.c  (or marrh.c) */
+// Thomas Wilson
+// gcc canny.c -o canny -lm
+// canny.exe garb34.pgm output1.pgm ignore.pgm 1 0
+#include <stdio.h>
 #include <math.h>
-#define  PICSIZE 256
-#define  MAXMASK 100
+#include <stdlib.h>
+#define PICSIZE 256
+#define MAXMASK 100
 
-         int    pic[PICSIZE][PICSIZE];
-         double outpic1[PICSIZE][PICSIZE];
-         double outpic2[PICSIZE][PICSIZE];
-         int    edgeflag[PICSIZE][PICSIZE];
-         double mask[MAXMASK][MAXMASK];
-         double conv[PICSIZE][PICSIZE];
+int pic[PICSIZE][PICSIZE];
+double outpicx[PICSIZE][PICSIZE];
+double outpicy[PICSIZE][PICSIZE];
+int edgeflag[PICSIZE][PICSIZE];
+double maskx[MAXMASK][MAXMASK];
+double masky[MAXMASK][MAXMASK];
+double ival[PICSIZE][PICSIZE];
 
-main(argc,argv)
-int argc;
-char **argv;
-{
-        int     i,j,p,q,s,t,mr,centx,centy;
-        double  maskval,sum,sig,maxival,minival,maxval,ZEROTOL;
-        FILE    *fo1, *fo2,*fp1, *fopen();
-        char    *foobar;
+int main(int argc, char **argv) {
+    int i,j,p,q,s,t,mr,centx,centy;
+    double  maskvalx,maskvaly,sumx,sumy,sig,maxival,minival,maxval,ZEROTOL;
+    FILE *fo1, *fo2,*fp1;
+    char *foobar;
 
-        argc--; argv++;
-        foobar = *argv;
-        fp1=fopen(foobar,"rb");
+    argc--; argv++;
+    foobar = *argv;
+    fp1=fopen(foobar,"rb");
 
-        argc--; argv++;
-        foobar = *argv;
-        fo1=fopen(foobar,"wb");
+    argc--; argv++;
+    foobar = *argv;
+    fo1=fopen(foobar,"wb");
 
-        argc--; argv++;
-        foobar = *argv;
-        fo2=fopen(foobar,"wb");
+    argc--; argv++;
+    foobar = *argv;
+    fo2=fopen(foobar,"wb");
 
-        argc--; argv++;
-        foobar = *argv;
-        sig = atof(foobar);
+    argc--; argv++;
+    foobar = *argv;
+    sig = atof(foobar);
 
-        argc--; argv++;
-        foobar = *argv;
-        ZEROTOL = atof(foobar);
+    argc--; argv++;
+    foobar = *argv;
+    ZEROTOL = atof(foobar);
 
-        mr = (int)(sig * 3);
-        centx = (MAXMASK / 2);
-        centy = (MAXMASK / 2);
+    mr = (int)(sig * 3);
+    centx = (MAXMASK / 2);
+    centy = (MAXMASK / 2);
 
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
-                {
-                  pic[i][j]  =  getc (fp1);
+    char header[256];
+    fgets(header, 256, fp1); 
+    fgets(header, 256, fp1);
+    fgets(header, 256, fp1);
+
+    for (i=0;i<256;i++) {
+        for (j=0;j<256;j++) {
+            pic[i][j] = getc (fp1);
+        }
+    }
+
+    for (p = -mr; p <= mr; p++) {
+        for (q = -mr; q <= mr; q++) {
+            maskvalx = (-(double)p / (sig*sig)) * exp(-(double)(p*p + q*q) / (2.0 * sig * sig));
+            maskvaly = (-(double)q / (sig*sig)) * exp(-(double)(p*p + q*q) / (2.0 * sig * sig));
+
+            (maskx[p+centy][q+centx]) = maskvalx;
+            (masky[p+centy][q+centx]) = maskvaly;
+        }
+    }
+
+    for (i = mr; i <= 255 - mr; i++) {
+        for (j = mr; j <= 255 - mr; j++) {
+            sumx = 0.0;
+            sumy = 0.0;
+            for (p=-mr;p<=mr;p++) {
+                for (q=-mr;q<=mr;q++) {
+                    sumx += pic[i+p][j+q] * maskx[p+centy][q+centx];
+                    sumy += pic[i+p][j+q] * masky[p+centy][q+centx];
                 }
+            }
+            outpicx[i][j] = sumx;
+            outpicy[i][j] = sumy;
         }
+    }
 
-        for (p=-mr;p<=mr;p++)
-        {  for (q=-mr;q<=mr;q++)
-           {
-              maskval = ((2-(((p*p)+(q*q))/(sig*sig)))*
-                      (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-              (mask[p+centy][q+centx]) = maskval;
-           }
+    maxival = 0;
+    for (i = mr; i < 256 - mr; i++) {
+        for (j = mr; j < 256 - mr; j++) {
+            ival[i][j]=sqrt((double)((outpicx[i][j] * outpicx[i][j]) + (outpicy[i][j] * outpicy[i][j])));
+            if (ival[i][j] > maxival) {
+                maxival = ival[i][j];
+            }
         }
+    }
+    
+    fprintf(fo1, "P5\n");
+    fprintf(fo1, "%d %d\n", PICSIZE, PICSIZE);
+    fprintf(fo1, "255\n");
 
-        for (i=mr;i<=255-mr;i++)
-        { for (j=mr;j<=255-mr;j++)
-          {
-             sum = 0;
-             for (p=-mr;p<=mr;p++)
-             {
-                for (q=-mr;q<=mr;q++)
-                {
-                   sum += pic[i+p][j+q] * mask[p+centy][q+centx];
-                }
-             }
-             outpic1[i][j] = sum;
-             conv[i][j] = sum;
-          }
+    for (i = 0; i < 256; i++) {
+        for (j = 0; j < 256; j++) {
+            ival[i][j] = (ival[i][j] / maxival) * 255;            
+            fprintf(fo1,"%c",(char)((int)(ival[i][j])));    
         }
-
-        maxval  = 0;
-        maxival = 0;
-        minival = 255;
-
-        for (i=mr;i<256-mr;i++)
-        { for (j=mr;j<256-mr;j++)
-          {
-             if (outpic1[i][j] > maxival)
-                maxival = outpic1[i][j];
-             if (outpic1[i][j] < minival)
-                minival = outpic1[i][j];
-           }
-        }
-        if (fabs(maxival) > fabs(minival))
-           maxval = fabs(maxival);
-        else
-           maxval = fabs(minival);
-
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
-          {
-             outpic1[i][j] = ((((outpic1[i][j]) / maxval) + 1) * 127);
-             fprintf(fo1,"%c",(char)((int)(outpic1[i][j])));
-          }
-        }
-
-        for (i=mr;i<=255-mr;i++)
-        {  for (j=mr;j<=255-mr;j++)
-           {
-                         outpic2[i][j] = 0;
-             if (conv[i][j] > ZEROTOL)
-             {
-               for (p=-1;p<=1;p++)
-               {
-                 for (q=-1;q<=1;q++)
-                 {
-                   if (conv[i+p][j+q] < -ZEROTOL)
-                   {
-                     outpic2[i][j] = 255;
-                   }
-                 }
-               }
-             }
-             else if ((fabs)(conv[i][j]) < ZEROTOL)
-             {
-                     if (((conv[i+1][j] > ZEROTOL) &&
-                          (conv[i-1][j] < -ZEROTOL))   ||
-                         ((conv[i+1][j] < -ZEROTOL) &&
-                          (conv[i-1][j] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i][j+1] > ZEROTOL) &&
-                               (conv[i][j-1] < -ZEROTOL))   ||
-                              ((conv[i][j+1] < -ZEROTOL) &&
-                               (conv[i][j-1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i+1][j+1] > ZEROTOL) &&
-                               (conv[i-1][j-1] < -ZEROTOL))   ||
-                              ((conv[i+1][j+1] < -ZEROTOL) &&
-                               (conv[i-1][j-1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i+1][j-1] > ZEROTOL) &&
-                               (conv[i-1][j+1] < -ZEROTOL))   ||
-                              ((conv[i+1][j-1] < -ZEROTOL) &&
-                               (conv[i-1][j+1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-             }
-           }
-        }
-
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
-          {  if (outpic2[i][j] == 255) outpic2[i][j]=0;
-             else outpic2[i][j]=255;
-             fprintf(fo2,"%c",(char)((int)(outpic2[i][j])));
-          }
-        }
+    }
 }
